2e875503c094c12ded048e4c9556f251bb4d064d
Pablo Matheus <pablo.lima.araujo@gmail.com>
2023-11-26 16:45:52 -0300

    Creating dev and test profiles

--- src/test/java/com/pablomatheus/purchase/controller/PurchaseControllerTest.java
+++ src/test/java/com/pablomatheus/purchase/controller/PurchaseControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -27,6 +28,7 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ActiveProfiles("test")
 @WebMvcTest(PurchaseController.class)
 class PurchaseControllerTest {
 
