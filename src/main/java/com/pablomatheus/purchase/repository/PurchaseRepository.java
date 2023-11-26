6540a1a07db6d0b4148c84ac2ae233cfb0155b67
Pablo Matheus <pablo.lima.araujo@gmail.com>
2023-11-26 12:28:59 -0300

    Adding PurchaseRepository

---
+++ src/main/java/com/pablomatheus/purchase/repository/PurchaseRepository.java
@@ -0,0 +1,7 @@
+package com.pablomatheus.purchase.repository;
+
+import com.pablomatheus.purchase.entity.PurchaseEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PurchaseRepository extends CrudRepository<PurchaseEntity, String> {
+}
